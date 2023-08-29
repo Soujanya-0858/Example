8a48e8398e928cdd0f510420f1d5e56d5359af7f
Soujanya <anipeddisoujanya@gmail.com>
2023-08-29 20:39:24 +0530

    maximum number

---
+++ assignment2/src/assignment2/maximumnum.java
@@ -0,0 +1,21 @@
+package assignment2;
+
+public class maximumnum {
+
+	public static void main(String[] args) {
+		int a= 0;
+		if(a>0) {
+			System.out.println("positivee");
+		}
+		else if(a<0)
+		{
+			System.out.println("negative");
+			
+		}
+		else {
+			System.out.println("zero");
+		}
+
+	}
+
+}
